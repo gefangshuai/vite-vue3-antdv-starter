850e8578899efb16b56620cc65165a3022c35539
gefangshuai <gefangshuai@163.com>
2022-04-19 06:26:02 +0000

    Deploying to gh-pages from  @ 2879d56579e720bd0fc14252b61571a4525952a8 🚀

---
+++ assets/useSize.3967c8b9.js
@@ -0,0 +1,1 @@
+import{c as i,z as o,aI as a,A as s}from"./index.f3f94cc1.js";var t=Symbol("SizeProvider"),c=function(e){var r=o("configProvider",a),n=i(function(){return e.size||r.componentSize});return s(t,n),n},z=function(e){var r=e?i(function(){return e.size}):o(t,i(function(){return"default"}));return r};export{c as a,z as u};
